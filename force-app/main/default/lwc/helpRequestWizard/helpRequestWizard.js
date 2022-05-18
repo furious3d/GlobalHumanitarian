220bb521cab7adaf8b8449410162e5d046167310
veronika <weronikashell@gmail.com>
2022-05-18 19:14:53 +0300

    change to custom labels

--- force-app/main/default/lwc/helpRequestWizard/helpRequestWizard.js
+++ force-app/main/default/lwc/helpRequestWizard/helpRequestWizard.js
@@ -1,14 +1,22 @@
 import { LightningElement, api } from 'lwc';
 import saveRequest from '@salesforce/apex/HelpRequestController.saveRequest';
 import btnSendRequest from '@salesforce/label/c.btnSendRequest';
+import textheading from '@salesforce/label/c.textheading';
+import firstName from '@salesforce/label/c.firstName';
+import lastName from '@salesforce/label/c.lastName';
+import Email from '@salesforce/label/c.Email';
 
 export default class HelpRequestWizard extends LightningElement {
     @api showCountryProp;
     fieldsValues = {};
     labels = {
-        btnSendRequest
+        btnSendRequest,
+        textheading,
+        firstName,
+        lastName,
+        Email
     };
-
+    typesofassistance=getRequestTypes();
     handleFieldChange(event) {
         const val = event.target.value;
         const name = event.target.name;
