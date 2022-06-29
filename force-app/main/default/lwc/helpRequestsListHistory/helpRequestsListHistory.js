ff4e0f551fabff489b50b3e4780a9f026f4412c7
veronika <weronikashell@gmail.com>
2022-06-29 16:09:59 +0300

    list of status and subject

--- force-app/main/default/lwc/helpRequestsListHistory/helpRequestsListHistory.js
+++ force-app/main/default/lwc/helpRequestsListHistory/helpRequestsListHistory.js
@@ -1,5 +1,14 @@
 import { LightningElement } from 'lwc';
 
 export default class HelpRequestListHistory extends LightningElement {
+    staSubHistory = []
 
+    loadHistory ()
+    {
+        findCases ()
+        .then(res => {
+            this.staSubHistory = res;
+            console.log (res);
+        });
+    }
 }
